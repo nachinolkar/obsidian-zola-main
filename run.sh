6213ef10e96e731f5dadcdaea167787686912169
nachinolkar <66353133+nachinolkar@users.noreply.github.com>
2023-07-19 19:24:53 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@ rsync -a __site/content/ __site/build/content
 
 # Use obsidian-export to export markdown content from obsidian
 mkdir -p __site/build/content/docs __site/build/__docs
+chmod a+x __site/bin/obsidian-export
+
 if [ -z "$STRICT_LINE_BREAKS" ]; then
 	__site/bin/obsidian-export --frontmatter=never --hard-linebreaks --no-recursive-embeds __obsidian __site/build/__docs
 else
